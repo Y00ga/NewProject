cbdbe4dee027c208e35b59e1f72e5ff616584fdf
Y00ga <91623972+Y00ga@users.noreply.github.com>
2021-09-30 01:42:02 +0700

    Delete NewProject directory

--- NewProject/web/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$SERVER = "localhost";
-$USER = "root";
-$PASSWORD = "";
-$nama_database ="contact";
-
-$db = mysqli_connect($SERVER, $USER, $PASSWORD, $nama_database) or die (mysqli_error($db));
-?>
--- NewProject/web/koneksidua.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host='localhost';
-$username='root';
-$password='';
-$database='db_tugas';
-
-$db=mysqli_connect($host, $username, $password, $database);
-?>
